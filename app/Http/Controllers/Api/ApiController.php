560982c3e14dacd399f230f13c3a2a118450bc62
Alphy <alpho07@gmail.com>
2023-08-19 12:28:13 +0300

    handle offset 0 API line 54

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -51,22 +51,27 @@ class ApiController extends Controller
     {
 
         $previous_reading = DB::select("SELECT current_reading FROM meter_readings WHERE client_id='$r->id' ORDER BY id DESC LIMIT 1");
-        $pr = $previous_reading[0]->current_reading;
+        
+        if (!empty($previous_reading)) {           
+            $pr = $previous_reading[0]->current_reading;
+        } else {
+            $pr = 0;
+        }
 
         $rate = Area::find($r->area_id)['rate'];
         $current_reading = $r->current_reading;
         $consumed = $current_reading - $pr;
         $total_cost = $consumed * $rate;
         $date =  date('Y-m-d H:i:s');
-        DB::insert("INSERT INTO meter_readings (client_id,reading_date,current_reading,previous_reading) VALUES ('$r->id','$date','$r->current_reading',$pr)");        
+        DB::insert("INSERT INTO meter_readings (client_id,reading_date,current_reading,previous_reading) VALUES ('$r->id','$date','$r->current_reading',$pr)");
         $date = date('Y-m-d H:i:s');
         $date1 = date_create($date);
         $date2 = date_format($date1, "M - Y");
-     
+
         $total_cost = $consumed * 120;
 
         $description = "WATER - $date2";
-        $ref='REF'.date('Ymd-His');
+        $ref = 'REF' . date('Ymd-His');
 
         DB::insert("INSERT INTO transactions (client_id,description,date,type,amount,units,last_read,previous_read,reference) VALUES ('$r->id','$description','$date','debit','$total_cost','$consumed','$current_reading','$pr','$ref')");
         DB::update("UPDATE meter_readings SET bill_run='1' WHERE client_id = '$r->id';");
