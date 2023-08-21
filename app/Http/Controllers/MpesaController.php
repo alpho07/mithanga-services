19a647d44094ebce1f3697a3654fd07d7d75613c
Alphy <alpho07@gmail.com>
2023-08-21 19:09:07 +0300

    w

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -56,7 +56,7 @@ class MpesaController extends Controller
         //Get Response data
          $response = Daraja::getInstance()->getDataFromCallback();
           DB::table('payment_dump')->insert([
-              'response'=>$response->all()
+              'response'=>response()->json($response)
           ]);
 
         // $response = $request->all(); //Alternatively...
