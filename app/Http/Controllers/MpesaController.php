88ac67cfa7f5f9ed216c8119eb248d057d0834a9
Alphy <alpho07@gmail.com>
2023-08-25 20:30:18 +0300

    yu

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -39,6 +39,7 @@ class MpesaController extends Controller
         $response = Daraja::getInstance()
             ->setCredentials("pUa9b2FKxxys2MEigOEQVfXmsfPNt7Kn", "iXMm01elw0YqPhtA", $environment)
             ->c2b($shortcode, $commandID, $amount, $msisdn, $billRefNumber);
+            Log::info('Formatted JSON:', ['json' =>   'I hit here']);
         //return json_decode($response,TRUE);
     }
 
